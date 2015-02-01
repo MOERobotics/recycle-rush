5002243eedd819325fe02d9df849a99193a3ea05
Kevin West <kwest@udel.edu>
2015-02-01 13:48:36 -0500

    Adding basic testbed code to test talons for continuity

--- JavaRobot/src/org/usfirst/frc/team365/robot/Robot.java
+++ JavaRobot/src/org/usfirst/frc/team365/robot/Robot.java
@@ -6,6 +6,10 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.Relay.Direction;
 import edu.wpi.first.wpilibj.Relay.Value;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import java.util.ArrayList;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -15,52 +19,67 @@ import edu.wpi.first.wpilibj.Relay.Value;
  * directory.
  */
 public class Robot extends IterativeRobot {
-	Joystick stick;
-	//int autoLoopCounter;
-	Relay relay;
-	CANTalon talon;
-	
-	
+
+    CANTalon[] talons = new CANTalon[16];
+    String[] talon_name = new String[16];
+    DriverStation ds;
+
+    public Robot() {
+        ds = DriverStation.getInstance();
+        for (int i = 0; i < 16; i++) {
+            try {
+                talons[i] = new CANTalon(i);
+                talon_name[i] = talons[i].getDescription();
+            } catch (RuntimeException e) {
+                talons[i]=null;
+                talon_name[i] = e.toString();
+            }
+            SmartDashboard.putString("talon_name"+i, talon_name[i]);
+        }
+    }
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-    	
-    	talon = new CANTalon(1);
-    	stick = new Joystick(0);
-    	relay = new Relay(0);
-    	
+
+
     }
     
     /**
      * This function is run once each time the robot enters autonomous mode
      */
     public void autonomousInit() {
-    	//autoLoopCounter = 0;
-    	talon.set(0);
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-    	talon.set(1);
+
     }
     
     /**
      * This function is called once each time the robot enters tele-operated mode
      */
     public void teleopInit(){
+        for(int i = 0; i < 16; i++) {
+            talons[i].set(0);
+            SmartDashboard.putNumber("Talons"+i,0.0);
+        }
     }
 
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-    	relay.setDirection(Direction.kForward);
-    	relay.set(Value.kOn);
-        talon.set(stick.getY());
+        for(int i = 0; i < 16; i++) if (talons[i] != null) {
+            talons[i].set(
+                limit(
+                    (float)SmartDashboard.getNumber("Talons"+i)
+                )
+            );
+        }
     }
     
     /**
@@ -69,5 +88,14 @@ public class Robot extends IterativeRobot {
     public void testPeriodic() {
     	//LiveWindow.run();
     }
+
+    public float limit(float in, float max) {
+        return (in > max)
+                ? max
+                : (in < -max)
+                ? -max
+                : in;
+    }
+    public float limit(float in) {return limit(in,1f);}
     
 }
